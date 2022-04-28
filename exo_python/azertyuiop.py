685199f5e3b44ddbb64eb17bfe0cb3e18da504f2
Nichodk <nicholas.de-kergorlay@enac.fr>
2022-04-28 15:12:02 +0200

    C'est ok fraï

--- exo_python/azertyuiop.py
+++ exo_python/azertyuiop.py
@@ -3,9 +3,9 @@
 from fileFlo import sound
 from file_coco import func
 
-print("Gneeeeeeeeeeeeeeee")
+print("Salut, c'est cool")
 sound()
 func()
 
-print("zbeuuuuuuuuuuuuuul")
+print("blop blop")
 
